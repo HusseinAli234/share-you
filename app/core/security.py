759c821da48d155caa49166f5c4fe635506b7342
HusseinAli234 <hussejnali3@gmail.com>
2026-07-27 15:27:07 +0600

    init

---
+++ app/core/security.py
@@ -0,0 +1,14 @@
+from passlib.context import CryptContext
+
+secure_context = CryptContext(schemes=["argon2"])
+
+
+def hashed_password(raw_password: str):
+    hashed = secure_context.hash(raw_password)
+    return hashed
+
+def verify(raw_password:str, hashed_password: str) -> bool:
+    return secure_context.verify(raw_password,hashed_password)
+
+
+    
