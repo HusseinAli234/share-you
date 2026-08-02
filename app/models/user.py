78370d9a3510a78644ab989c3591bae2564605d3
HusseinAli234 <hussejnali3@gmail.com>
2026-08-02 23:28:57 +0600

    init db

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import func
+from datetime import datetime
+
+from app.db.base import Base
+from app.db.base import TimestampMixin
+
+class User(TimestampMixin, Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    login: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str] = mapped_column()
