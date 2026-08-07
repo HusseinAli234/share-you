7c79a95d4e25616c1cdb94b1d6e3489c2fa31ee5
HusseinAli234 <hussejnali3@gmail.com>
2026-08-07 16:08:36 +0600

    expand doc and project models

--- app/models/documents.py
+++ app/models/documents.py
@@ -8,8 +8,15 @@ from app.db.base import Base, TimestampMixin
 
 class Document(TimestampMixin, Base):
     __tablename__ = "documents"
+    
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    project_id: Mapped[int] = mapped_column(ForeignKey("projects.id"))
+    name: Mapped[str] = mapped_column(nullable=False)
+    size: Mapped[int] = mapped_column()
+    s3_key: Mapped[str] = mapped_column(nullable=False)
+    doc_type: Mapped[str] = mapped_column()
+    
+    
+    project_id: Mapped[int] = mapped_column(ForeignKey("projects.id", ondelete="CASCADE"))
 
     project: Mapped["Project"] = relationship(back_populates="documents")
